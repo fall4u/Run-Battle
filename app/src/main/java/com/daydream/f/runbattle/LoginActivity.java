a1c2e7046a56b72239a7c206ef2532d980a8d19e
fall4u <fall4ufeng@gmail.com>
2016-06-22 15:02:58 +0800

    add user info activity/UI

--- app/src/main/java/com/daydream/f/runbattle/LoginActivity.java
+++ app/src/main/java/com/daydream/f/runbattle/LoginActivity.java
@@ -1,17 +1,19 @@
 package com.daydream.f.runbattle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    boolean isPass = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -20,6 +22,19 @@ public class LoginActivity extends AppCompatActivity {
         weChatLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { // use weChat ID to login
+                EditText editText = (EditText)findViewById(R.id.Email);
+                String account = editText.getText().toString();
+
+                editText = (EditText)findViewById(R.id.passWord);
+                String passWord = editText.getText().toString();
+
+                // TODO: verify account and password in server
+                if(isPass){
+                   Intent intent = new Intent(LoginActivity.this, UserActivity.class);
+                    intent.putExtra("account", account);
+                    startActivity(intent);
+                    //jump to user information UI
+                }
                 Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
             }
         });
---
+++ app/src/main/java/com/daydream/f/runbattle/UserActivity.java
@@ -0,0 +1,26 @@
+package com.daydream.f.runbattle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String account = intent.getStringExtra("account");
+        Log.d("UserActivity", account);
+
+    }
+
+}
